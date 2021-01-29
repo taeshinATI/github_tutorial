8be39b883bdbd31e749daca1956813a5e16f53b3
tspark <tspark@ati2000.co.kr>
2021-01-29 09:32:03 +0900

    changed by tspark

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 #test file for github tutotial
-
+def test():
+    print('hi')
 if __name__ == "__main__":
     print("github tutorial")
+    print('qwer asdf zxcv')
+    print('asdfasdfsaf')
+    test()
